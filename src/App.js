22d4f0c93030641e1eb91ad61a2d528b8dab7c53
vijay131094 <Vijaykumar8008280683@gmail.com>
2022-02-28 12:13:58 +0530

    containers and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+    <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Testimonial/>
+      <Skills/>
+      <Footer/>
+
+
+    </div>
   )
 }
 
