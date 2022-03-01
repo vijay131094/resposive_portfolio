442e957a1c398dee4c61a860c48f06623132c67c
vijay131094 <Vijaykumar8008280683@gmail.com>
2022-03-02 01:28:06 +0530

    Skills and WorkExperience completed and MotionWrap is added

--- src/container/Work/Work.js
+++ src/container/Work/Work.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {AiFillEye,AiFillGithub} from 'react-icons/ai'
 import './Work.scss'
 import { motion } from 'framer-motion'
-import { AppWrap } from '../../wrapper'
+import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
 
 
@@ -132,4 +132,7 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+   'work',
+   "app__primarybg")
